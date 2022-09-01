9b8f65495253c27e3c106ef8991e11fce939f08e
Celeste JS <wonderlugosi@gmail.com>
2022-09-01 09:17:26 -0500

    cambios

---
+++ client/src/components/botonprueba.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+export default function BPrueba() {
+    return (
+        <div>
+            <Button>
+                Boton
+            </Button>
+        </div>
+    )
+}
